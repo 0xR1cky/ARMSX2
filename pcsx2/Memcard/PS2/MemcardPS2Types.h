30242ff927335d78eecab311daac84857f78b57e
RedPanda4552 <redpanda4552@gmail.com>
2021-12-18 21:23:18 -0500

    Add larger memory card capacities

--- pcsx2/Memcard/PS2/MemcardPS2Types.h
+++ pcsx2/Memcard/PS2/MemcardPS2Types.h
@@ -42,7 +42,15 @@ enum class EraseBlockSize
 // Size of a memcard, counted in sectors. Datatype is u32.
 enum class SectorCount
 {
-	STANDARD = 0x00004000
+	STANDARD = 0x00004000,	// 8 MiB
+	X2 = 0x00008000,		// 16 MiB
+	X4 = 0x00010000,		// 32 MiB
+	X8 = 0x00020000,		// 64 MiB
+	X16 = 0x00040000,		// 128 MiB
+	X32 = 0x00080000,		// 256 MiB
+	X64 = 0x00100000,		// 512 MiB
+	X128 = 0x00200000,		// 1 GiB
+	X256 = 0x00400000		// 2 GiB
 };
 
 constexpr size_t ECC_BYTES = 16;
